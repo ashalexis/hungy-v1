e06e0dc8e6c32db3bb45cce958f9c05809e5f64d
Ashley Lim <ashalexis1297@gmail.com>
2022-06-09 22:18:41 +1000

    first foray

---
+++ README.md
@@ -0,0 +1,16 @@
+# Let's decide!
+
+Have you and your partner(s) ever struggled to decide on what to eat?
+This site was created for this very purpose, so no one will ever have to pull hair over food decisions ever again! :)
+
+## v0.1
+
+### Made with:
+
+- HTML, CSS, Vanilla JS
+- Express
+- Socket IO
+
+## Special Shout-outs
+
+Tim please stop asking me what to eat everyday dangit
---
+++ index.js
@@ -0,0 +1,25 @@
+// set up the express server
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+// set up socket io
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+// listening on the connection event
+io.on("connection", (socket) => {
+	socket.on("food choice", (msg) => {
+		console.log("message: ", msg);
+		io.emit("food choice", msg);
+	});
+});
+
+server.listen(3121, () => {
+	console.log("listening on port 3121");
+});

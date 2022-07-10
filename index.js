95b3a7f629d9945a51035886f13a7aedc7b17b6f
Ashley Lim <ashalexis1297@gmail.com>
2022-07-10 16:03:03 +1000

    package json + formatting

--- index.js
+++ index.js
@@ -11,41 +11,41 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/elimination", (req, res) => {
-	res.sendFile(__dirname + "/elimination.html");
+    res.sendFile(__dirname + "/elimination.html");
 });
 
 const rooms = {};
 
 // listening on the connection event
 io.on("connection", (client) => {
-	// making new room
-	client.on("newRoom", () => {
-		let roomId = uuid();
-		rooms[client.id] = roomId;
-		client.emit("roomCode", roomId);
+    // making new room
+    client.on("newRoom", () => {
+        let roomId = uuid();
+        rooms[client.id] = roomId;
+        client.emit("roomCode", roomId);
 
-		client.join(roomId);
-		client.emit("init");
-	});
+        client.join(roomId);
+        client.emit("init");
+    });
 
-	// join room
-	client.on("joinRoom", (roomId) => {
-		// const room = io.sockets.adapter.rooms[roomId];
-		rooms[client.id] = roomId;
-		client.emit("roomCode", roomId);
-		client.join(roomId);
-		client.emit("init");
-	});
+    // join room
+    client.on("joinRoom", (roomId) => {
+        // const room = io.sockets.adapter.rooms[roomId];
+        rooms[client.id] = roomId;
+        client.emit("roomCode", roomId);
+        client.join(roomId);
+        client.emit("init");
+    });
 
-	client.on("food choice", (choice, roomId) => {
-		io.emit("food choice", choice, roomId);
-	});
+    client.on("food choice", (choice, roomId) => {
+        io.emit("food choice", choice, roomId);
+    });
 });
 
 server.listen(3121, () => {
-	console.log("listening on port 3121");
+    console.log("listening on port 3121");
 });

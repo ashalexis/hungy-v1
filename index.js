0e53c6f2ddf5d140971812d6513869bc785aa804
Ashley Lim <ashalexis1297@gmail.com>
2022-07-10 15:43:48 +1000

    rooms feature added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { uuid } = require("uuidv4");
+
 // set up the express server
 const express = require("express");
 const app = express();
@@ -12,14 +14,35 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/elimination', (req, res) => {
-	res.sendFile(__dirname + '/elimination.html')
-})
+app.get("/elimination", (req, res) => {
+	res.sendFile(__dirname + "/elimination.html");
+});
+
+const rooms = {};
 
 // listening on the connection event
-io.on("connection", (socket) => {
-	socket.on("food choice", (choice) => {
-		io.emit("food choice", choice);
+io.on("connection", (client) => {
+	// making new room
+	client.on("newRoom", () => {
+		let roomId = uuid();
+		rooms[client.id] = roomId;
+		client.emit("roomCode", roomId);
+
+		client.join(roomId);
+		client.emit("init");
+	});
+
+	// join room
+	client.on("joinRoom", (roomId) => {
+		// const room = io.sockets.adapter.rooms[roomId];
+		rooms[client.id] = roomId;
+		client.emit("roomCode", roomId);
+		client.join(roomId);
+		client.emit("init");
+	});
+
+	client.on("food choice", (choice, roomId) => {
+		io.emit("food choice", choice, roomId);
 	});
 });
 

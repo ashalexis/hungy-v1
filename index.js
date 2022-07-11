44c45cbd8c32b49196398596241ec72ff4e63e07
Ashley Lim <ashalexis1297@gmail.com>
2022-07-11 13:35:29 +1000

    elimination page + error handling

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const { uuid } = require("uuidv4");
+import { nanoid } from "nanoid";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // set up the express server
-const express = require("express");
+import express from "express";
 const app = express();
-const http = require("http");
-const server = http.createServer(app);
+const server = createServer(app);
 
 // set up socket io
-const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.get("/elimination", (req, res) => {
-    res.sendFile(__dirname + "/elimination.html");
+    res.sendFile(path.join(__dirname, "/elimination.html"));
 });
 
 const rooms = {};
@@ -23,8 +29,8 @@ const rooms = {};
 // listening on the connection event
 io.on("connection", (client) => {
     // making new room
-    client.on("newRoom", () => {
-        let roomId = uuid();
+    client.on("createRoom", () => {
+        let roomId = nanoid(8);
         rooms[client.id] = roomId;
         client.emit("roomCode", roomId);
 
@@ -34,11 +40,21 @@ io.on("connection", (client) => {
 
     // join room
     client.on("joinRoom", (roomId) => {
-        // const room = io.sockets.adapter.rooms[roomId];
-        rooms[client.id] = roomId;
-        client.emit("roomCode", roomId);
-        client.join(roomId);
-        client.emit("init");
+        const room = io.sockets.adapter.rooms.get(roomId);
+        if (!room) {
+            client.emit("throwError", {
+                status: 404,
+                message: "Room not found!",
+                roomId,
+            });
+        } else if (room.size > 1) {
+            client.emit("throwError", { status: 400, message: "Room full!", roomId });
+        } else {
+            rooms[client.id] = roomId;
+            client.emit("roomCode", roomId);
+            client.join(roomId);
+            client.emit("init");
+        }
     });
 
     client.on("food choice", (choice, roomId) => {

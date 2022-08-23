b4e41fcb2e292840b9078f135b2cb107e01d29f9
Ashley Lim <ashalexis1297@gmail.com>
2022-08-23 21:03:41 +1000

    matchup edit choices - no admin

--- js/helpers.js
+++ js/helpers.js
@@ -1,46 +1,48 @@
 const getMostVoted = (map) => {
-    const maxVotes = Math.max(...Array.from(map.values()));
-    return Array.from(map.entries()).find(([, val]) => val == maxVotes)[0];
+	const maxVotes = Math.max(...Array.from(map.values()));
+	return Array.from(map.entries()).find(([, val]) => val == maxVotes)[0];
 };
 
 const getTopThree = (map) => {
-    const sortedEntries = Array.from(map.entries()).sort(([, valA], [, valB]) => {
-        return valB - valA;
-    });
-    return sortedEntries.slice(0, 3).map(([key]) => key);
+	const sortedEntries = Array.from(map.entries()).sort(([, valA], [, valB]) => {
+		return valB - valA;
+	});
+	return sortedEntries.slice(0, 3).map(([key]) => key);
 };
 
 const getRandom = (map) => {
-    const index = Math.floor(Math.random() * map.size);
-    return Array.from(map.keys())[index];
+	const index = Math.floor(Math.random() * map.size);
+	return Array.from(map.keys())[index];
 };
 
 export const getMatchupAnswer = (answerChoice, allAnswers) => {
-    if (!allAnswers || !answerChoice) return;
+	if (!allAnswers || !answerChoice) return;
 
-    const answersArray = Object.values(allAnswers).flat();
-    const flattenedAnswers = new Map();
+	const answersArray = Object.entries(allAnswers)
+		.filter(([key]) => key !== "allFoodOptions")
+		.flatMap(([, value]) => value);
+	const flattenedAnswers = new Map();
 
-    answersArray.forEach((val) => {
-        if (flattenedAnswers.get(val)) {
-            flattenedAnswers.set(val, flattenedAnswers.get(val) + 1);
-        } else {
-            flattenedAnswers.set(val, 1);
-        }
-    });
+	answersArray.forEach((val) => {
+		if (flattenedAnswers.get(val)) {
+			flattenedAnswers.set(val, flattenedAnswers.get(val) + 1);
+		} else {
+			flattenedAnswers.set(val, 1);
+		}
+	});
 
-    switch (answerChoice.toLowerCase()) {
-        case "most voted":
-            return getMostVoted(flattenedAnswers);
-        case "top three":
-            return getTopThree(flattenedAnswers);
-        case "random":
-            return getRandom(flattenedAnswers);
-        default:
-            return;
-    }
+	switch (answerChoice.toLowerCase()) {
+		case "most voted":
+			return getMostVoted(flattenedAnswers);
+		case "top three":
+			return getTopThree(flattenedAnswers);
+		case "random":
+			return getRandom(flattenedAnswers);
+		default:
+			return;
+	}
 };
 
 export const removeDuplicates = (array) => [
-    ...new Map(array.map((item) => [item["id"], item])).values(),
+	...new Map(array.map((item) => [item["id"], item])).values(),
 ];
